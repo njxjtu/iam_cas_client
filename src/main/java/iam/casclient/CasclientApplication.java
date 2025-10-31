243449926be25ac6f14a1f75e22e66c92b325749
Nan Jiang <njxjtu@gmail.com>
2025-10-30 22:32:06 -0700

    Initial checkin

---
+++ src/main/java/iam/casclient/CasclientApplication.java
@@ -0,0 +1,13 @@
+package iam.casclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasclientApplication.class, args);
+	}
+
+}
